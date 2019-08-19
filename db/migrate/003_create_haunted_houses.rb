32fafa1664f916f162a535956456ad307de5219a
Nicole Dow <nikki.dow71@gmail.com>
2019-08-19 20:56:27 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   
   
   def change
+    create_table :haunted_houses do |t|
     t.string :name
     t.string :location
     t.string :theme
@@ -12,6 +13,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
     t.datetime :closing_date
     t.string :long_description
     t.timestamps
+  end
   end
   
 end
