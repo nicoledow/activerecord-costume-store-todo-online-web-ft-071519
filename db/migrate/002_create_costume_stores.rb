45a521750d1049b4664f271254a58f0fae1d8f58
Nicole Dow <nikki.dow71@gmail.com>
2019-08-19 20:53:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,6 +9,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
+      t.datetime :closing_time
+      t.timestamps
     end
   end
   
